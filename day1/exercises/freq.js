d9033cc7f0851569c84aea76f7bcf48da676d0c9
alessiamarcolini <98marcolini@gmail.com>
2019-11-06 14:51:19 +0100

    Add first two exercises of day1

--- day1/exercises/freq.js
+++ day1/exercises/freq.js
@@ -1,1 +1,34 @@
-/* Implement your solution here */
+/*
+ * freq.js
+ * Create a frequency table from an input file containing on word per line.
+ */
+
+// Loading the file system library
+const fs = require('fs');
+
+// File name from the common line params
+const fileName = process.argv[2];
+
+// Accessing the content of the file asynchnously
+
+let data = fs.readFileSync(fileName, 'utf8');
+
+var splitted = data.split('\n');
+var frequencies = {}
+
+for (let i = 0; i < splitted.length; i++){
+    //console.log(splitted[i]);
+    var tag = splitted[i];
+    if (!(tag in frequencies)){
+        frequencies[tag] = 1;
+    }
+    else{
+        frequencies[tag] = frequencies[tag]+1;
+    }
+} 
+console.log('Tag\tFrequency');
+
+for (let tag in frequencies){
+    console.log(tag + '\t' + frequencies[tag]);
+}
+
--- day1/exercises/two-numbers.js
+++ day1/exercises/two-numbers.js
@@ -1,1 +1,16 @@
+/*
+ * two-numbers.js
+ * Implement two-numbers.js, verifying that the user provides the right number of parameters (2) and in the right format (numbers).
+ */
+
+// Loading the file system library
+const fs = require('fs');
+
+// Check number of params
+if (process.argv.length == 2){
+  console.log('Number of params OK!');
+}
+else{
+  console.log('Number of params NOT OK!');
+}
 
